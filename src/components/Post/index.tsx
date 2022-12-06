df3f44c709b072f9dfe01fa4a64b2462b6a1c376
Peterson Munuera <peterson.munuera@somosdx.co>
2022-12-06 06:06:24 -0300

    feat: get data from API

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,11 +1,85 @@
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+
 import styles from './styles.module.scss'
 
-export function Post() {
+type PostProps = {
+  id: number,
+  userId: number,
+  title: string,
+  body: string
+}
+
+type CommentProps = {
+  id: number,
+  postId: number,
+  name: string,
+  email: string,
+  body: string
+}
+
+type AuthorProps = {
+  name: string
+}
+
+export function Post(props: PostProps) {
+  const [comments, setComments] = useState<CommentProps[]>([])
+  const [author, setAuthor] = useState<AuthorProps>({} as AuthorProps)
+  const [showComments, setShowComments] = useState(false)
+
+  useEffect(() => {
+    getComments(props.id)
+    getAuthor(props.userId)
+  }, [])
+
+  async function getComments(postId: number) {
+    try {
+      const { data: commentsData } = await api.get(`/posts/${postId}/comments`)
+
+      if (!commentsData) return
+
+      setComments(commentsData)
+    } catch (error) {
+      console.log(`Erro ao buscar comentários do post: ${error}`)
+    }
+  }
+
+  async function getAuthor(userId: number) {
+    try {
+      const { data: userData } = await api.get(`/users/${userId}`)
+
+      if (!userData) return
+
+      setAuthor(userData)
+    } catch (error) {
+      console.log(`Erro ao buscar usuário autor do post: ${error}`)
+    }
+  }
+
+  function toggleComments() {
+    setShowComments(!showComments)
+  }
+
   return (
     <li className={styles.card}>
-      <h1>Titulo do post</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, saepe! Sit eaque assumenda commodi perferendis earum, odit labore sed? Et deserunt culpa non, labore facilis harum soluta repellendus ad nulla.</p>
-      <small>5 comentários</small>
+      <span>{author.name}</span>
+      <h1>{props.title}</h1>
+      <p>{props.body}</p>
+
+      <button onClick={toggleComments} className={showComments ? styles.buttonActive : ''}>
+        <small>{!showComments ? 'Visualizar' : 'Ocultar'} {comments.length} comentários</small>
+      </button>
+
+      {showComments && (
+        <div className={styles.comments}>
+          {comments.map(comment => (
+            <div key={comment.id}>
+              <h3>{comment.email}</h3>
+              <p>{comment.body}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </li>
   )
 }
--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,16 +1,46 @@
+import { useState, useEffect } from "react"
+import { api } from "../../services/api"
+
 import { Post } from "../Post"
 import styles from './styles.module.scss'
 
+type PostProps = {
+  id: number,
+  userId: number,
+  title: string,
+  body: string
+}
+
 export function Timeline() {
+  const [posts, setPosts] = useState<PostProps[]>([])
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
+  async function getPosts() {
+    try {
+      const { data: postsData } = await api.get('/posts')
+      
+      if (!postsData) return
+  
+      setPosts(postsData)
+    } catch (error) {
+      console.log(`Erro ao buscar posts: ${error}`)
+    }
+  }
+
   return (
     <ul className={styles.container}>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {posts.map(post => (
+        <Post 
+          key={post.id}
+          id={post.id}
+          userId={post.userId}
+          title={post.title}
+          body={post.body}
+        />
+      ))}
     </ul>
   )
 }
